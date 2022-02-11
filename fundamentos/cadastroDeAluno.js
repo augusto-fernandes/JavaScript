1c78da95ac32bb2d892adf4b9792a3c0c6d34086
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2022-02-11 20:55:49 -0300

    primeiro commit

---
+++ fundamentos/cadastroDeAluno.js
@@ -0,0 +1,9 @@
+const aluno = {
+    nome: "Matheus",
+    Sobrenome: "fraga",
+    idade: 26 ,
+    apelido: "sujeitoprogramador",
+    email: "teste@teste.com",
+    status:true,
+}
+console.log(aluno)
